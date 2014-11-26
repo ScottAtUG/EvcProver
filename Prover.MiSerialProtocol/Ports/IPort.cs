fff7792263fc4ee6ac24658c1ac1bb21f5e562c4
mcquaiga <adam.mcquaig@edocgroup.com>
2014-11-25 20:24:07 -0800

    checkpoint

---
+++ Prover.MiSerialProtocol/Ports/IPort.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prover.MiSerialProtocol.Ports
+{
+    public interface IPort
+    {
+        bool IsOpen();
+        void OpenPort();
+        void ClosePort();
+        void Dispose();
+        int BytesToRead();
+        string Read();
+        string Write(string data);
+        void DiscardInBuffer();
+    }
+}
---
+++ Prover.MiSerialProtocol/Ports/IrDAPort.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prover.MiSerialProtocol.Ports
+{
+    class IrDAPort : IPort
+    {
+        public bool IsOpen()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OpenPort()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ClosePort()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int BytesToRead()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Write(string data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DiscardInBuffer()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
